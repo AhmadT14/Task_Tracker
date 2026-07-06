27805e7f7f345445c0d08de976f1c360c6bfded6
AhmadT14 <ahragh@gmail.com>
2026-07-06 14:30:13 +0300

    Implement task update functionality and refactor add task logic

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
 #!/usr/bin/env node
-import { Command } from 'commander'
-import { addtask } from './addCommand.js'
-const program = new Command()
-
-program.name('task-cli').description('tracking and managing your tasks').version('0.0.1')
+import { Command } from "commander";
+import { addtask } from "./addCommand.js";
+import { updateTask } from "./updateCommand.js";
+const program = new Command();
 
 program
-  .command('add')
-  .description('Add a given task')
-  .argument('<taskDescription>', 'task to add')
+  .name("task-cli")
+  .description("tracking and managing your tasks")
+  .version("0.0.1");
+
+program
+  .command("add")
+  .description("Add a given task")
+  .argument("<taskDescription>", "task to add")
   .action((taskDescription) => {
-    addtask(taskDescription)
+    addtask(taskDescription);
   })
-program.parse()
+  program
+  .command("update")
+  .description("update a specific task")
+  .argument("<taskID>", "taskID to update")
+  .argument("<newTaskDescription>", "new task description")
+  .action((taskID, newTaskDescription) => {
+    updateTask(taskID, newTaskDescription);
+  });
+program.parse();
