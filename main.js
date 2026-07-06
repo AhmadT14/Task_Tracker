90f47d2afb1d4588c9d3afb6058a921cac8a94ef
AhmadT14 <ahragh@gmail.com>
2026-07-06 14:46:39 +0300

    Add commands to mark tasks as in progress and done

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ import { Command } from "commander";
 import { addtask } from "./addCommand.js";
 import { updateTask } from "./updateCommand.js";
 import { deleteTask } from "./deleteCommand.js";
+import { markInProgress } from "./markInProgressCommand.js";
+import { markDone } from "./markDoneCommand.js";
 const program = new Command();
 
 program
@@ -25,11 +27,25 @@ program
   .action((taskID, newTaskDescription) => {
     updateTask(taskID, newTaskDescription);
   });
-    program
+  program
   .command("delete")
   .description("delete a specific task")
   .argument("<taskID>", "taskID to delete")
   .action((taskID) => {
     deleteTask(taskID);
+  });
+    program
+  .command("mark-in-progress")
+  .description("mark a specific task as in progress")
+  .argument("<taskID>", "taskID to mark as in progress")
+  .action((taskID) => {
+    markInProgress(taskID);
+  });
+    program
+  .command("mark-done")
+  .description("mark a specific task as done")
+  .argument("<taskID>", "taskID to mark as done")
+  .action((taskID) => {
+    markDone(taskID);
   });
 program.parse();
---
+++ markDoneCommand.js
@@ -0,0 +1,42 @@
+import fs from "node:fs";
+
+const filePath = "./newUser.json";
+let found=false;
+function markDone(taskID) {
+  if (fs.existsSync(filePath)) {
+    fs.readFile(filePath, "utf8", (err, data) => {
+      if (err) {
+        console.error("Error reading file:", err);
+        return;
+      }
+      try {
+        const jsonObject = JSON.parse(data);
+        for (let task of jsonObject.tasks) {
+          if (task.id === Number(taskID)) {
+            task.status = "done";
+            task.updatedAt = new Date().toString();
+            found = true;
+            break;
+          }
+        }
+        if (found) {
+        const updatedJson = JSON.stringify(jsonObject, null, 2);
+        fs.writeFile(filePath, updatedJson, "utf8", (err) => {
+          if (err) {
+            console.error("Error writing file:", err);
+            return;
+          }
+          console.log("JSON file successfully updated!");
+        });
+    } else {
+      console.error("Task not found");
+    }
+      } catch (parseErr) {
+        console.error("Error parsing JSON string:", parseErr);
+      }
+    });
+  } else {
+    console.error("File not found");
+  }
+}
+export { markDone };
